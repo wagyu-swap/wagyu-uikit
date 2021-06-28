a5ffc8b00cd4027fe059169021a86bec4f58a8cc
danny-jin <danny@cabinetex.com>
2021-06-28 21:11:02 +0900

    remove: all wallet except Metamask update: AccountModal with Velas TestNet

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
+      <LinkExternal small href={`https://evmexplorer.testnet.velas.com/address/${account}`} mr="16px">
+        View on Velas TestNet Explorer
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
